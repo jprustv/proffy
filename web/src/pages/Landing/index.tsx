e8af7f99a9550f8cbd11860109191d58e34c395a
Jonathan Prust Vernizzi <jonathan@flipfloplab.com>
2020-08-09 11:20:52 -0300

    Adicionadas rotas /study e /give-classes

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -7,6 +7,8 @@ import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
+import { Link } from 'react-router-dom'
+
 import './styles.css';
 
 function Landing() {
@@ -22,14 +24,14 @@ function Landing() {
         <img src={landingImg} alt="Plataforma de estudos" className="hero-image"/>
 
         <div className="buttons-container">
-          <a href="" className="study">
+          <Link to="/study" className="study">
             <img src={studyIcon} alt="Estudar"/>
             Estudar
-          </a>
-          <a href="" className="give-classes">
+          </Link>
+          <Link to="/give-classes" className="give-classes">
             <img src={giveClassesIcon} alt="Dar aula"/>
             Dar aula
-          </a>
+          </Link>
         </div>
 
         <span className="total-connections">
