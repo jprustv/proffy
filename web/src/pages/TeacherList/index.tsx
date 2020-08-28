4e10edaee63f8d100afd72330270f1b57ad643bf
Jonathan Prust Vernizzi <jonathan@flipfloplab.com>
2020-08-27 21:37:23 -0300

    Feito teacher item dentro da TeacherList. Próximo passo será colocar num componente próprio.

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -4,6 +4,8 @@ import './styles.css'
 
 import PageHeader from '../../components/PageHeader'
 
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
@@ -23,6 +25,34 @@ function TeacherList() {
           </div>
         </form>
       </PageHeader>
+
+      <main>
+        <article className="teacher-item">
+          <header>
+            <img src="https://avatars3.githubusercontent.com/u/11440704?s=460&u=47b433d1818ef1f3deddaa35ed9a830dfef8a2ba&v=4" alt="Jonathan Prust Vernizzi"/>
+            <div>
+              <strong>Jonathan Prust Vernizzi</strong>
+              <span>Unity 3D</span>
+            </div>
+          </header>
+          <p>
+            Ensina desenvolvimento de jogos utilizando Unity3D e linguagem de programação C#.
+            <br /><br />
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu sem at est vulputate tincidunt vitae id ante. Pellentesque at tortor tincidunt, interdum tellus id, luctus nibh. Praesent lobortis leo.
+          </p>
+          <footer>
+            <p>
+              Preço/Hora
+              <strong>R$ 80,00</strong>
+            </p>
+            <button type="button">
+              <img src={whatsappIcon} alt="Whatsapp"/>
+              Entrar em contato
+            </button>
+          </footer>
+        </article>
+      </main>
+
     </div>
   )
 }
