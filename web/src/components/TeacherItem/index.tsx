6d7f87c87dc54b2873e8e8cda79147f0eff02f52
Jonathan Prust Vernizzi <jonathan@flipfloplab.com>
2020-08-28 16:39:34 -0300

    Feita contagem de conexoes ao clicar no botão do whatsapp. Concluído App Web. Próxima aula começa o App em react-native

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -4,6 +4,8 @@ import './styles.css'
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
+import api from '../../services/api'
+
 export interface Teacher {
   id : number,
   name : string,
@@ -19,6 +21,13 @@ interface TeacherItemProps {
 }
 
 const TeacherItem : React.FC<TeacherItemProps> = ( { teacher }) => {
+
+  function createNewConnection(){
+    api.post('connections', {
+      user_id : teacher.id
+    })
+  }
+
   return (
     <article className="teacher-item">
       <header>
@@ -36,7 +45,11 @@ const TeacherItem : React.FC<TeacherItemProps> = ( { teacher }) => {
           Preço/Hora
           <strong>R$ {teacher.cost}</strong>
         </p>
-        <a href={`https://wa.me/${teacher.whatsapp}`}>
+        <a
+          target="_blank"
+          onClick={createNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt="Whatsapp"/>
           Entrar em contato
         </a>
