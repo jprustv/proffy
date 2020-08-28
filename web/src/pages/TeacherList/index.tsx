0c273f4a9c4c0160291b4e1a749a4f36d1bdee91
Jonathan Prust Vernizzi <jonathan@flipfloplab.com>
2020-08-28 14:31:14 -0300

    Select colocado na matéria e dia da semana do TeacherList

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -5,14 +5,35 @@ import './styles.css'
 import PageHeader from '../../components/PageHeader'
 import TeacherItem from '../../components/TeacherItem'
 import Input from '../../components/Input'
+import Select from '../../components/Select'
 
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis">
         <form id="search-teachers">
-          <Input name="subject" label="Matéria"/>
-          <Input name="week_day" label="Dia da semana"/>
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value : 'Javacript', label : 'Javacript' },
+              { value : 'PHP', label : 'PHP' },
+              { value : 'C#', label : 'C#' }
+            ]}
+          />
+          <Select
+            name="week_day"
+            label="Dia da semana"
+            options={[
+              { value : '0', label : 'Domingo' },
+              { value : '1', label : 'Segunda' },
+              { value : '2', label : 'Terça' },
+              { value : '3', label : 'Quarta' },
+              { value : '4', label : 'Quinta' },
+              { value : '5', label : 'Sexta' },
+              { value : '6', label : 'Sábado' }
+            ]}
+          />
           <Input name="time" label="Hora" type="time"/>
         </form>
       </PageHeader>
