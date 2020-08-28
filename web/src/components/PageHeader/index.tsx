0ed44c329901d66bca7a09e7d34b5d1461b3adc5
Jonathan Prust Vernizzi <jonathan@flipfloplab.com>
2020-08-28 13:21:00 -0300

    Adicionada a prop description (opcional) no page header

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -8,7 +8,8 @@ import backIcon from '../../assets/images/icons/back.svg'
 import './styles.css'
 
 interface PageHeaderProps {
-  title : string
+  title : string,
+  description? : string
 }
 
 const PageHeader : React.FC<PageHeaderProps> = (props) => {
@@ -22,6 +23,7 @@ const PageHeader : React.FC<PageHeaderProps> = (props) => {
       </div>
       <div className="header-content">
         <strong>{props.title}</strong>
+        {props.description && <p>{props.description}</p>}
         {props.children}
       </div>
     </header>
